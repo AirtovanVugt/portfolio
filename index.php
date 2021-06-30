1792759af0a7a578f86d0aee2e295842b5b4b7fa
AirtovanVugt <70692336+AirtovanVugt@users.noreply.github.com>
2021-06-30 12:19:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<script src="lingo/lingo-nl.js"></script>
+</head>
+<body>
+
+	<h1>Portfolio Airto</h1>
+
+	<hr>
+
+	<h2>Lingo</h2>
+
+	<div id="lingo-container">
+	  	<label for="hetRaden">Raden:</label>
+	  	<input type="text" id="raden" name="hetRaden"><br><br>
+	  	<button id="checkKnop">check</button>
+	  	<br><br>
+	</div>
+
+	<p>Hier heb ik kleine aplicatie gemaakt in javascript</p>
+
+  	<script src="lingo/lingo.js"></script>
+
+  	<hr>
+
+  	<h2></h2>
+
+</body>
+</html>
